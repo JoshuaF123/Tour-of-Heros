d67a362b00261a136e65a828212016d55ef53f58
JoshuaF123 <50995598+JoshuaF123@users.noreply.github.com>
2022-10-06 15:16:51 -0400

    Thinkering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HeroesComponent} from './heroes/heroes.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
