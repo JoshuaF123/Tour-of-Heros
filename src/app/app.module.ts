e847ed6dcccd2175072d01ece97d8acb08863e83
JoshuaF123 <50995598+JoshuaF123@users.noreply.github.com>
2022-10-06 15:35:26 -0400

    Fixed the logic errors preventing load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroesComponent} from './heroes/heroes.component';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
   imports:      [ BrowserModule, FormsModule ],
   bootstrap:    [ AppComponent ]
 })
